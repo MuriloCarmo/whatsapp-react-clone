3cb1185ef4a7355533cdae88086a1dbf1b4fc2e0
MuriloCarmo <murilo1258@hotmail.com>
2021-07-16 17:53:25 -0300

    Uploading files to WhatsApp React Clone

---
+++ src/components/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Login.css';
+import Api from '../Api';
+
+function Login({onReceive}) {
+
+    const handleFacebookLogin = async () => {
+        let result = await Api.fbPopup();
+        if (result) {
+            onReceive(result.user);
+        } else {
+            alert("Erro!");
+        }
+    }
+
+    return (
+        <div className="login">
+            <button onClick={handleFacebookLogin}>Logar com o Facebook</button>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBL6b0VSQAxlO8dccaYexBvq4gZ4nIuFWM",
+    authDomain: "whatsappclone-1dc9d.firebaseapp.com",
+    projectId: "whatsappclone-1dc9d",
+    storageBucket: "whatsappclone-1dc9d.appspot.com",
+    messagingSenderId: "98765957395",
+    appId: "1:98765957395:web:1bc87e437369bab29352e6"
+};
